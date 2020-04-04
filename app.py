cd988c713baedc9ba678e3bd6d817ead163b73fc
TJ Salgado <tsalgado96@gmail.com>
2020-04-04 21:27:25 +0200

    Templates using bootstrap.

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'TJ Salgado',
+        'title': 'Blog Post 1',
+        'content': 'First post content.',
+        'date_posted': 'April 4th, 2020'
+    },
+    {
+        'author': 'TJ Salgado',
+        'title': 'Blog Post 1',
+        'content': 'First post content.',
+        'date_posted': 'April 4th, 2020'
+    }
+]
 
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
